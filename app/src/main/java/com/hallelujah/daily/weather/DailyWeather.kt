f327360ec64d51054eaf4af4d6ee693660352a2b
lydiasama <hallelujah.lydia@gmail.com>
2018-06-20 16:59:42 +0700

    add Hawk for shared preference

---
+++ app/src/main/java/com/hallelujah/daily/weather/DailyWeather.kt
@@ -0,0 +1,12 @@
+package com.hallelujah.daily.weather
+
+import android.app.Application
+import com.orhanobut.hawk.Hawk
+
+class DailyWeather: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        Hawk.init(this).build()
+    }
+}
