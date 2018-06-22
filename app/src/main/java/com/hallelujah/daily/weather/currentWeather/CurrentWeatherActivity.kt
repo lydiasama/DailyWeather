04f98769d9525beb1afcc2981fe319502e1763af
lydiasama <hallelujah.lydia@gmail.com>
2018-06-23 00:01:51 +0700

    add current weather activity

---
+++ app/src/main/java/com/hallelujah/daily/weather/currentWeather/CurrentWeatherActivity.kt
@@ -0,0 +1,13 @@
+package com.hallelujah.daily.weather.currentWeather
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.hallelujah.daily.weather.R
+
+class CurrentWeatherActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_current_weather)
+    }
+}
