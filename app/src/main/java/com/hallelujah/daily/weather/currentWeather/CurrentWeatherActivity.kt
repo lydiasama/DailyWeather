eb39f2017dda7cc5235dde338bdfbe115a6a137c
lydiasama <hallelujah.lydia@gmail.com>
2018-06-23 18:39:39 +0700

    update view current weather

--- app/src/main/java/com/hallelujah/daily/weather/currentWeather/CurrentWeatherActivity.kt
+++ app/src/main/java/com/hallelujah/daily/weather/currentWeather/CurrentWeatherActivity.kt
@@ -3,25 +3,30 @@ package com.hallelujah.daily.weather.currentWeather
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import com.hallelujah.daily.weather.CITY
-import com.hallelujah.daily.weather.HUMIDITY
-import com.hallelujah.daily.weather.R
-import com.hallelujah.daily.weather.TEMP
+import com.hallelujah.daily.weather.*
 import com.hallelujah.daily.weather.forecast.ForecastActivity
 import com.orhanobut.hawk.Hawk
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_current_weather.*
 
 class CurrentWeatherActivity : AppCompatActivity() {
     private lateinit var city : String
-
     private lateinit var humidity : String
     private lateinit var temp : String
+    private lateinit var iconName : String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_current_weather)
+        getIconNameFromeIntent()
+        setupIconWeather()
         getCurrentWeatherFromSharedPreference()
         setupView()
         setOnClickListener()
+    }
+
+    private fun getIconNameFromeIntent() {
+        this.iconName = intent.getStringExtra(INTENT_ICON_NAME)
     }
 
     private fun getCurrentWeatherFromSharedPreference() {
@@ -33,6 +38,11 @@ class CurrentWeatherActivity : AppCompatActivity() {
     private fun setupView() {
         tvTemp.text = temp
         tvHumidity.text = getString(R.string.humidity, humidity)
+    }
+
+    private fun setupIconWeather() {
+        val url = "http://openweathermap.org/img/w/$iconName.png"
+        Picasso.with(this).load(url).into(ivWeather)
     }
 
     private fun setOnClickListener() {
