4d51f1bc60015aed84fc41f241b2afb7d0cc935f
lydiasama <hallelujah.lydia@gmail.com>
2018-06-23 15:36:33 +0700

    create retrofit on create app

--- app/src/main/java/com/hallelujah/daily/weather/DailyWeather.kt
+++ app/src/main/java/com/hallelujah/daily/weather/DailyWeather.kt
@@ -1,11 +1,17 @@
 package com.hallelujah.daily.weather
 
 import android.app.Application
+import com.hallelujah.daily.weather.core.network.createRetrofit
 import com.orhanobut.hawk.Hawk
+import retrofit2.Retrofit
 
 class DailyWeather : Application() {
+
+    lateinit var client: Retrofit
+
     override fun onCreate() {
         super.onCreate()
         Hawk.init(this).build()
+        client = createRetrofit()
     }
 }
